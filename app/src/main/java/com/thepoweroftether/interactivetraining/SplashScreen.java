6276983c0609b47fadaa1bd3bc46b3056ad23351
AdolfFG <adolf.filius@student.umn.ac.id>
2016-12-07 20:25:25 +0700

    New Splash Screen

--- app/src/main/java/com/thepoweroftether/interactivetraining/SplashScreen.java
+++ app/src/main/java/com/thepoweroftether/interactivetraining/SplashScreen.java
@@ -1,36 +1,167 @@
+//package com.thepoweroftether.interactivetraining;
+//
+//import android.content.Intent;
+//import android.os.Handler;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class SplashScreen extends AppCompatActivity {
+//    // Splash screen timer
+//    private static int SPLASH_TIME_OUT = 1700;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash_screen);
+//
+//        new Handler().postDelayed(new Runnable() {
+//
+//            /*
+//             * Showing splash screen with a timer. This will be useful when you
+//             * want to show case your app logo / company
+//             */
+//
+//            @Override
+//            public void run() {
+//                // This method will be executed once the timer is over
+//                // Start your app main activity
+//                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+//                startActivity(i);
+//
+//                // close this activity
+//                finish();
+//            }
+//        }, SPLASH_TIME_OUT);
+//    }
+//}
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 package com.thepoweroftether.interactivetraining;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.support.v4.app.ActivityManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 
 public class SplashScreen extends AppCompatActivity {
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 1700;
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
 
+    /** Called when the activity is first created. */
+    Thread splashTread;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        StartAnimation1();
+    }
 
-        new Handler().postDelayed(new Runnable() {
+    private void StartAnimation1() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.linlaySplashScreen);
+        l.clearAnimation();
+        l.startAnimation(anim);
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.imgLogo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
 
+        splashTread = new Thread() {
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(i);
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this,
+                            LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen.this.finish();
+                }
 
-                // close this activity
-                finish();
             }
-        }, SPLASH_TIME_OUT);
+        };
+        splashTread.start();
+
     }
+
+    private void StartAnimation2() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.beta);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.linlaySplashScreen);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.imgLogo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this,
+                            LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+
+    }
+
 }
